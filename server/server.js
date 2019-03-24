43ae66430fed9703424b29fcb648f21ad33b772c
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-03-25 00:12:28 +0530

    Initialized the repo

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+
+
+app.listen(
+    port,
+    () => {
+        console.log(`Listening to port ${port}`);
+    }
+)
