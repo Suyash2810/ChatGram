c1f413e5c64e742597461d9b3ffa13e61ef993e5
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-03-26 22:24:43 +0530

    Added ui display of messages.

--- public/js/script.js
+++ public/js/script.js
@@ -13,12 +13,26 @@ socket.on(
     'newMessage',
     function (newMessage) {
         console.log("New message has been received.", newMessage);
+
+        var li = document.createElement('li');
+        var text = document.createTextNode(`From: ${newMessage.from}, Text: ${newMessage.text}`);
+        li.appendChild(text);
+        document.getElementById('list_users').appendChild(li);
     }
 );
 
-socket.emit('createMessage', {
-    from: "Tallis",
-    text: "Hello people."
-}, function (data) {
-    console.log(data);
-});
+window.onload = function () {
+
+    document.getElementById('message_form').addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        var text = document.getElementById('text_field').value;
+        socket.emit('createMessage', {
+            from: "User",
+            text: text,
+            timeStamp: new Date().getTime()
+        }, function (data) {
+            console.log(data);
+        });
+    });
+}
